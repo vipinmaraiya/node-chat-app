348587947a74509aa964a9cbf2121a83f6dc1e66
Vipin Sharma <vipinsharmaec@hotmail.com>
2018-03-23 00:07:26 +0530

    Socket connect and disconnect event

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 const path = require("path"),
 express = require("express"),
-app = express();
+app = express(),
+socketIO = require("socket.io"),
+http = require("http");
+
+var server = http.createServer(app);
+
+var io = socketIO(server);
+
+io.on("connection", (socket) =>{
+    console.log("New User connected")
+
+    socket.on("disconnect", ()=>{
+        console.log("User was disconnected");
+    })
+});
+
 const publicPath = path.join(__dirname,
     "../public");
 app.use(express.static(publicPath));
 
-app.get("/", (req, res) =>{
-    
-});
-
-app.listen(3000, ()=>{
+server.listen(3000, ()=>{
     console.log(`Server is running on port 3000`);
 });
